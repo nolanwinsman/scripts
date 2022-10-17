22286138be8d217af6b6654587fc1268032634b5
Nolan <68390327+nolanwinsman@users.noreply.github.com>
2022-10-17 13:07:00 -0600

    put everything inside main

--- qbittorrent_organizer.py
+++ qbittorrent_organizer.py
@@ -23,28 +23,33 @@ PATHS = {
         "anime-movie": f"{DIR}/anime-movies/"
 }
 
-# instantiate a Client using the appropriate WebUI configuration
-# change information to fit your login
-qbt_client = qbittorrentapi.Client(
-    host='localhost',
-    port=8080,
-    username='admin',
-    password='adminadmin'
- )
+def main():
 
-# the Client will automatically acquire/maintain a logged in state in line with any request.
-# therefore, this is not necessary; however, you many want to test the provided login credentials.
-try:
-    qbt_client.auth_log_in()
-except qbittorrentapi.LoginFailed as e:
-    print(e)
+    # instantiate a Client using the appropriate WebUI configuration
+    # change information to fit your login
+    qbt_client = qbittorrentapi.Client(
+        host='localhost',
+        port=8080,
+        username='admin',
+        password='adminadmin'
+    )
 
-# retrieve all torrents and set their download folder to the corresponding tag
-for torrent in qbt_client.torrents_info():
-    # print(f'{torrent.name} ({torrent.state}) ({torrent.tags})')
+    # the Client will automatically acquire/maintain a logged in state in line with any request.
+    # therefore, this is not necessary; however, you many want to test the provided login credentials.
+    try:
+        qbt_client.auth_log_in()
+    except qbittorrentapi.LoginFailed as e:
+        print(e)
 
-    # torrent tag is present
-    if len(torrent.tags) > 0 and torrent.tags in PATHS:
-        p = f"{PATHS[torrent.tags]}"
-        print(f"Setting {torrent.name} location to {p}")
-        torrent.set_location(p)
+    # retrieve all torrents and set their download folder to the corresponding tag
+    for torrent in qbt_client.torrents_info():
+        # print(f'{torrent.name} ({torrent.state}) ({torrent.tags})')
+
+        # torrent tag is present
+        if len(torrent.tags) > 0 and torrent.tags in PATHS:
+            p = f"{PATHS[torrent.tags]}"
+            print(f"Setting {torrent.name} location to {p}")
+            torrent.set_location(p)
+
+if __name__ == "__main__":
+    main()
